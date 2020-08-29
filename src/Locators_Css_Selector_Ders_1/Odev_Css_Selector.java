851ec159edba8c2bf0aac613f0ca0e92b49c9cfc
omerozdemir72 <omerozdemir7274@gmail.com>
2020-08-29 12:42:02 +0200

    Css Selector My Store Add  to Cart Test

---
+++ src/Locators_Css_Selector_Ders_1/Odev_Css_Selector.java
@@ -0,0 +1,42 @@
+package Locators_Css_Selector_Ders_1;
+
+public class Odev_Css_Selector {
+    /*
+Ödev:
+            https://demoqa.com/automation-practice-form   bu siteye gidin.
+todo        SADECE aşağıda bahsedilen kısımları doldurunuz !
+1.  Name
+2.    LastName
+3.      Email
+4.      Gender:
+5.      Hobbies
+6.      Current adress
+7.      submit butona tıkla
+8.      Ekrandaki mesajı dogrula
+9.      2 Saniye bekledikten sonra close butonuna bas
+10.   2 saniye bekleyip sistemi kapat.
+
+            name,id, classname ve kendinizi geliştirmek için daha çok CSS SELECTOR ile aramalar yapın.
+
+
+            Buraları kendinize göre doldurun.
+            Submit butonuna bastırın.
+
+             yeni bir bilgi çerçevesi açılacaktır.
+             Bilgi çerçevesinin başlığının ekranda gözüküp gözükmediğini check edin. (Eğer gözüktüyse formda zorunlu olan kısımları doldurmuşsunuz demektir,yani sorun yok demektir.)
+
+             Bilgi mesajı : Thanks for submitting the form
+            Onu görünce de close butonuna bastırın ve bekleyip sistemi tamamen kapatın.
+
+            Test başarıyla tamamlandıysa konsolda "Formu doldurdugunuz için teşekkür ederiz..." mesajı yazsın.
+
+
+
+
+
+
+        todo   NOT: CSS SELECTOR ile arama yaparken input, button, label gibi tag leri de iyi inceleyin.
+            Bu test ısınma içindir. CSS selector pazartesi günü daha detaylı görülecektir.
+
+ */
+}
